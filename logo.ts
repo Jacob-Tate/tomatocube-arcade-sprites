9c9b6de68c93145a6e17a2473954b06d0ff51bf0
Tomato Cube <47109215+TomatoCube18@users.noreply.github.com>
2020-05-31 10:18:49 +0800

    Add files via upload

--- logo.ts
+++ logo.ts
@@ -1,4 +1,4 @@
 namespace tomatocube {
     //% fixedInstance jres blockIdentity=images._image
-    export const logoLogo = image.ofBuffer(hex``);
+    export const logoColored = image.ofBuffer(hex``);
 }
